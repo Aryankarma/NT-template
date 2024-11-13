978cc749ee1e35267a6c96e6da9743ce2accf069
aryandatacode <aryan.datacode@gmail.com>
2024-11-13 15:02:04 +0530

    initial setup next, tailwind, typescript

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center align-center min-w-screen min-h-screen">
+	<h1 className="max-h-content">This is Next + Typescript template</h1>
+    </div>
+  );
+}
